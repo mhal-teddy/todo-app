d510e5491feda654c0e6db0c03a4245e14c55eb1
mhal-teddy <y.tada@estyle-inc.jp>
2024-07-14 18:16:18 +0900

    [add] HTTP server

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import http from "node:http";
+import router from "./todoRoutes";
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+    } else {
+        router(req, res);
+    }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
