f0e0835c22d54a048e7a903e1a2bb57f4ff3309e
mhal-teddy <y.tada@estyle-inc.jp>
2024-07-14 22:26:29 +0900

    [add] frontend

---
+++ frontend/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded');
+
+function fetchTodos() {
+    fetch('http://127.0.0.1:3000/api/todos')
+    .then(response => response.json())
+    .then(todos => {
+        const todoList = document.getElementById('todo-list');
+        todoList.innerHTML = '';
+        todos.forEach(todo => {
+            const li = document.getElementById('li');
+            li.textContent = li.title;
+            todoList.appendChild(li);
+        });
+    });
+};
+
+const addBtn = document.getElementById('add-btn');
+addBtn.addEventListener('click', () => {
+    const newTodo = document.getElementById('new-todo').value;
+    fetch('http://127.0.0.1:3000/api/todos', {
+        method: 'POST',
+        headers: {
+            'ContentType': 'application/json'
+        },
+        body: JSON.stringify({title: newTodo}),
+    })
+    .then(response => response.json())
+    .then(todo => {
+        const todoList = document.getElementById('todo-list');
+        const li = document.getElementById('li');
+        li.textContent = todo.title;
+        todoList.appendChild(li);
+        document.getElementById('new-todo').value = '';
+    });
+});
