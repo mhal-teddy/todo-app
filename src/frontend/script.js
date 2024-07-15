4b2258885866cd6528df4df47c3beed0f3eb090c
mhal-teddy <y.tada@estyle-inc.jp>
2024-07-15 09:35:04 +0900

    [fix] UI

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -7,16 +7,23 @@ function fetchTodos() {
         const todoList = document.getElementById('todo-list');
         todoList.innerHTML = '';
         todos.forEach(todo => {
-            const li = document.getElementById('li');
-            li.textContent = li.title;
+            const li = document.createElement('li');
+            li.textContent = todo.title;
             todoList.appendChild(li);
         });
     });
 };
 
 const addBtn = document.getElementById('add-btn');
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
     const newTodo = document.getElementById('new-todo').value;
+    if (newTodo === '') {
+        alert('Please input text.');
+        return;
+    }
+
     fetch('http://127.0.0.1:3000/api/todos', {
         method: 'POST',
         headers: {
@@ -27,7 +34,7 @@ addBtn.addEventListener('click', () => {
     .then(response => response.json())
     .then(todo => {
         const todoList = document.getElementById('todo-list');
-        const li = document.getElementById('li');
+        const li = document.createElement('li');
         li.textContent = todo.title;
         todoList.appendChild(li);
         document.getElementById('new-todo').value = '';
